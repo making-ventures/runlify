715df4ede4dc86aeb89f2696fcaab2c334c4e304
Damir <tebuchet@gmail.com>
2022-06-13 22:12:13 +0300

    start

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -23,12 +23,8 @@ module.exports = {
     const envName = envDefined
       ? parameters.first.replace('env=', '')
       : config?.developer?.defaultEnvironment
-    toolbox.print.info('envName')
-    toolbox.print.info(envName)
-    toolbox.print.info('config?.developer?.defaultEnvironment')
-    toolbox.print.info(config?.developer?.defaultEnvironment)
-    toolbox.print.info('config')
-    toolbox.print.info(config)
+    // toolbox.print.info('envName')
+    // toolbox.print.info(envName)
 
     if (envName) {
       nconf.file({
