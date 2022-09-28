78ed5c428f05b4d307b2b8775e82df9a7c7ac567
Damir <tebuchet@gmail.com>
2022-09-28 13:40:23 +0300

    uniqueKey

--- src/projectsGeneration/modules/addEmailModuleEntities.ts
+++ src/projectsGeneration/modules/addEmailModuleEntities.ts
@@ -64,7 +64,7 @@ const addEmailModuleEntities = (system: SystemMetaBuilder) => {
   mailingMessages.addField('subject').setType('string');
   mailingMessages.addLinkField('mailingMessageStatuses', 'mailingMessageStatusId').setType('string').setRequired();
   mailingMessages.addLinkField('messageTemplateLangVariants', 'messageTemplateLangVariantId').setType('int');
-  mailingMessages.addUniqueConstraint(['mailingCampaignId', 'to', 'localsHash']);
+  mailingMessages.addUniqueConstraint(['mailingCampaignId', 'to', 'uniqueKey']);
 };
 
 export default addEmailModuleEntities;
