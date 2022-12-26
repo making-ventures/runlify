d3d8be5498bcf89163ce6a615806ecc0ef7f5429
Damir <tebuchet@gmail.com>
2022-12-26 20:58:13 +0300

    ConfigVars

--- src/projectsGeneration/generators/fileTemplates/back/environment/docs/backDocsConfiguration.ts
+++ src/projectsGeneration/generators/fileTemplates/back/environment/docs/backDocsConfiguration.ts
@@ -26,8 +26,8 @@ If both files exists they will be merged. \`\${ENV}.json\` takes precedence.
 ## Configuration variables
 
 ${markdownTable([
-  ['Key in file', 'Environment', 'Type', 'Default value', 'Need for'],
-  ...backVars.map((v) => [v.name, constantCase(v.name), v.type, v.default, v.needFor]),
+  ['Key in file', 'Environment', 'Type', 'Required', 'Default value', 'Need for'],
+  ...backVars.map((v) => [v.name, constantCase(v.name), v.type, v.required, v.default, v.needFor]),
 ])}
 `
 }
