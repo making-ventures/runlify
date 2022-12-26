02b68e2b6f09cd26eabc080656aa5b719543b310
Damir <tebuchet@gmail.com>
2022-12-27 00:43:25 +0300

    Configs

--- src/projectsGeneration/generators/fileTemplates/back/root/config/index.ts
+++ src/projectsGeneration/generators/fileTemplates/back/root/config/index.ts
@@ -54,6 +54,9 @@ if (exists(file)) {
 
 export const isLocalEnv = envName === 'local';
 
+type ValueBasedOnRequired<Req extends boolean, Val extends number | string | boolean | Date | bigint>
+  = Req extends true ? Val : Val | undefined
+
 export const getFromNconf = <T extends boolean>(name: string, required: T): ValueBasedOnRequired<T, string> => {
   const value = nconf.get(constantCase(name)) || nconf.get(name);
 
