f25a6b471517caaca4326d438ccd16256d7c1ee0
irinalwnko <irk1404@gmail.com>
2023-01-15 12:49:52 +0300

    update titles addRefreshTokens

--- src/projectsGeneration/commonEntities/addRefreshTokens.ts
+++ src/projectsGeneration/commonEntities/addRefreshTokens.ts
@@ -3,15 +3,50 @@ import SystemMetaBuilder from '../builders/SystemMetaBuilder';
 const addRefreshTokens = (system: SystemMetaBuilder) => {
   const appRefreshTokens = system.addCatalog('appRefreshTokens', {singular: 'App refresh token', plural: 'App refresh tokens'});
   appRefreshTokens.setNeedFor('Хранилище рефреш токенов для app');
-  appRefreshTokens.addField('create').setType('datetime').setRequired();
-  appRefreshTokens.addLinkField('users', 'userId').setType('int').setRequired();
-  appRefreshTokens.addField('token').setType('string').setRequired();
+  appRefreshTokens.addField('create')
+    .setTitles({
+      en: 'Create',
+      ru: 'Создать',
+    })
+    .setType('datetime')
+    .setRequired();
+  appRefreshTokens.addLinkField('users', 'userId')
+    .setTitles({
+      en: 'User',
+      ru: 'Пользователь',
+    })
+    .setType('int')
+    .setRequired();
+  appRefreshTokens.addField('token')
+  .setTitles({
+    en: 'Token',
+    ru: 'Токен',
+  })
+  .setType('string')
+  .setRequired();
 
   const uiRefreshTokens = system.addCatalog('admRefreshTokens', {singular: 'Ui refresh token', plural: 'Ui refresh tokens'});
   uiRefreshTokens.setNeedFor('Хранилище рефреш токенов для админа');
-  uiRefreshTokens.addField('create').setType('datetime').setRequired();
-  uiRefreshTokens.addLinkField('managers', 'managerId').setRequired();
-  uiRefreshTokens.addField('token').setType('string').setRequired();
+  uiRefreshTokens.addField('create')
+    .setTitles({
+      en: 'Create',
+      ru: 'Создать',
+    })
+    .setType('datetime')
+    .setRequired();
+  uiRefreshTokens.addLinkField('managers', 'managerId')
+    .setTitles({
+      en: 'Manager',
+      ru: 'Менеджер',
+    })
+    .setRequired();
+  uiRefreshTokens.addField('token')
+  .setTitles({
+    en: 'Token',
+    ru: 'Токен',
+  })
+  .setType('string')
+  .setRequired();
 };
 
 export default addRefreshTokens;
