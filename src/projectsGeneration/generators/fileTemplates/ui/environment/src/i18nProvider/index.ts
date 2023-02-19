55848c2b93baaea457182e9cc12b803edda42c44
Damir <tebuchet@gmail.com>
2023-02-20 00:12:56 +0300

    translation

--- src/projectsGeneration/generators/fileTemplates/ui/environment/src/i18nProvider/index.ts
+++ src/projectsGeneration/generators/fileTemplates/ui/environment/src/i18nProvider/index.ts
@@ -8,40 +8,9 @@ import { generatedWarning } from '../../../../../../utils'
 export const uiI18nProviderTmpl = (
   { system: { defaultLanguage } }: ProjectWideGenerationArgs,
   options: BootstrapEntityOptions = defaultBootstrapEntityOptions
-) => `import * as React from 'react';
-import {
-  useState, useEffect,
-} from 'react';
-import {
-  Admin,
-  AuthProvider,
-  useTranslate,
-  CustomRoutes,
-  localStorageStore,
-} from 'react-admin';
-import {
-  ApolloClient,
-  ApolloProvider,
-  NormalizedCacheObject,
-} from '@apollo/client';
-import polyglotI18nProvider from 'ra-i18n-polyglot';
-import './App.css';
-import Layout from './layout/Layout';
-import Login from './layout/Login';
-import defaultMessages from './i18n/${defaultLanguage}';
-import dataProviderFactory from './dataProvider';
-import getConfig from './config/config';
-import {createBrowserHistory} from 'history';
-import {getResources} from './adm/resources';
-import Dashboard from './adm/Dashboard';
-import {DebugProvider} from './contexts/DebugContext';
-import getAuthProvider from './authProvider/getAuthProvider';
-import {onStart} from './systemHooks';
-import getApollo from './apollo/getApollo';
-import Loader from './shared/Loader';
-import {lightTheme} from './layout/themes';
-import {routes} from './adm/routes';
-import log from './utils/log';
+) => `import polyglotI18nProvider from 'ra-i18n-polyglot';
+import defaultMessages from '../i18n/ru';
+import log from '../utils/log';
 ${
   options.skipWarningThisIsGenerated
     ? ''
@@ -49,10 +18,6 @@ ${
 // ${generatedWarning}
 `
 }
-onStart();
-
-const history = createBrowserHistory();
-
 const i18nProvider = polyglotI18nProvider(locale => {
   switch (locale) {
   case 'en':
@@ -65,61 +30,5 @@ const i18nProvider = polyglotI18nProvider(locale => {
   }
 }, 'ru');
 
-const App = () => {
-  const [dataProvider, setDataProvider] = useState<any | null>(null);
-  const [authProvider, setAuthProvider] = useState<AuthProvider | null>(null);
-  const [client, setClient] = useState<ApolloClient<NormalizedCacheObject> | null>(null);
-  const translate = useTranslate();
-
-  useEffect(() => {
-    const fetchDataProvider = async () => {
-      const config = await getConfig();
-      setAuthProvider(getAuthProvider(config.endpoint));
-
-      const client = getApollo(config.endpoint);
-      setClient(client);
-      const dataProviderInstance = await dataProviderFactory(client);
-      setDataProvider(() => dataProviderInstance);
-    };
-
-    fetchDataProvider();
-  }, []);
-
-  if (!dataProvider || !client || !authProvider) {
-    return (
-      <Loader />
-    );
-  }
-
-  return (
-    <ApolloProvider client={client}>
-      <DebugProvider>
-        <Admin
-          dashboard={Dashboard}
-          dataProvider={dataProvider}
-          history={history}
-          i18nProvider={i18nProvider}
-          layout={Layout}
-          loading={Loader}
-          loginPage={Login}
-          authProvider={authProvider}
-          title=''
-          theme={lightTheme}
-          store={localStorageStore('3')}
-        >
-          {permissions => [
-            (
-              <CustomRoutes key='customRoutes'>
-                {routes}
-              </CustomRoutes>
-            ),
-            ...getResources(translate, permissions),
-          ]}
-        </Admin>
-      </DebugProvider>
-    </ApolloProvider>
-  );
-};
-
-export default App;
+export default i18nProvider;
 `
