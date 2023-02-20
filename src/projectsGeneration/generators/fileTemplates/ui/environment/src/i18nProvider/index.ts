b9e65de2cbd8f4d12c945cce2c8fda7b985eb352
Damir <tebuchet@gmail.com>
2023-02-20 17:42:08 +0300

    locales

--- src/projectsGeneration/generators/fileTemplates/ui/environment/src/i18nProvider/index.ts
+++ src/projectsGeneration/generators/fileTemplates/ui/environment/src/i18nProvider/index.ts
@@ -9,6 +9,7 @@ export const uiI18nProviderTmpl = (
   { system: { defaultLanguage, languages } }: ProjectWideGenerationArgs,
   options: BootstrapEntityOptions = defaultBootstrapEntityOptions
 ) => `import polyglotI18nProvider from 'ra-i18n-polyglot';
+import {Locale} from 'ra-core';
 import defaultMessages from '../i18n/${defaultLanguage}';
 import log from '../utils/log';
 import {ValidationMessages} from '../i18n/types';
@@ -21,6 +22,10 @@ ${
 // ${generatedWarning}
 `
 }
+const locales: Locale[] = [${languages.map(({id, title}) => `
+  {locale: '${id}', name: '${title}'},`).join('')}
+],;
+
 const i18nProvider: I18nProvider = ({
   ...polyglotI18nProvider(
     locale => {
@@ -45,9 +50,7 @@ const i18nProvider: I18nProvider = ({
       }
     },
     'ru',
-    [${languages.map(({id, title}) => `
-      {locale: '${id}', name: '${title}'},`).join('')}
-    ],
+    locales,
   ),
   locales,
 });
