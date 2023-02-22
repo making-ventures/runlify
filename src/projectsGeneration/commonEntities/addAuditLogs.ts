441a84dcdac6158ba5f0ff15019a3948820f3aa3
tebuchet <tebuchet@gmail.com>
2023-02-23 00:00:00 +0300

    StringType

--- src/projectsGeneration/commonEntities/addAuditLogs.ts
+++ src/projectsGeneration/commonEntities/addAuditLogs.ts
@@ -1,4 +1,4 @@
-import { StringType } from '../../types'
+import { StringType } from '../builders/buildedTypes'
 import SystemMetaBuilder from '../builders/SystemMetaBuilder'
 
 const addAuditLogs = (system: SystemMetaBuilder) => {
