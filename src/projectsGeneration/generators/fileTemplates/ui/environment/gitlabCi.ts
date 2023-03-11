2e7c77d77189b6e9f73fe7f5ba479340a6ab6e95
Damir <tebuchet@gmail.com>
2023-03-11 16:46:04 +0300

    ci

--- src/projectsGeneration/generators/fileTemplates/ui/environment/gitlabCi.ts
+++ src/projectsGeneration/generators/fileTemplates/ui/environment/gitlabCi.ts
@@ -131,6 +131,7 @@ ${system.deployEnvironments
     DEV: "false"
     HOST: "making.ventures"
     ROOT_ENABLED: "true"
+    KUBE_CONFIG: \${KUBE_${e.clusterName.toUpperCase()}_CONFIG}
     TAG: ":\${CI_COMMIT_REF_SLUG}-\${CI_COMMIT_SHA}"${system.configVars
       .filter((v) => v.scopes.includes('admin-app') || v.scopes.includes('ci'))
       .map(
