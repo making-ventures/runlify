a5b7dab5d00a8f9c284f7f40eaf77ec04b68c24b
Damir <tebuchet@gmail.com>
2023-03-27 16:06:24 +0300

    s3.sstEnabled

--- src/projectsGeneration/builders/SystemMetaBuilder.ts
+++ src/projectsGeneration/builders/SystemMetaBuilder.ts
@@ -158,6 +158,13 @@ class SystemMetaBuilder {
       's3.eu-central-1.wasabisys.com',
       'Эндпоинт S3, который использует бекенд',
     )
+    this.addConfigVar(
+      's3.sstEnabled',
+      'bool',
+      false,
+      true,
+      'Включен ли ssl при работе с S3',
+    )
     // this.addConfigVar(
     //   's3.publicEndpoint',
     //   'string',
