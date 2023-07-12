2fbab21ae77be7d497c341b733e6f381fe533036
Tebuchet <tebuchet@gmail.com>
2023-07-12 08:27:42 +0300

    fix padding

--- src/projectsGeneration/generators/fileTemplates/back/graph/types.ts
+++ src/projectsGeneration/generators/fileTemplates/back/graph/types.ts
@@ -7,7 +7,16 @@ import { generatedWarning, pad1 } from '../../../../utils'
 export const backBaseTypesTmpl = (
   printedSchema: string,
   options: BootstrapEntityOptions = defaultBootstrapEntityOptions
-) => `import {gql} from 'apollo-server';
+) => {
+  const scheme = pad1(printedSchema.replaceAll('`', "'"));
+  const maxLenght = Math.max(...scheme.split('\n').map(el => el.length))
+
+  return `
+  ${
+    maxLenght > 140
+      ? ''
+      : `/* eslint-disable max-len */
+`}import {gql} from 'apollo-server';
 ${
   options.skipWarningThisIsGenerated
     ? ''
@@ -16,6 +25,6 @@ ${
 `
 }
 export default gql\`
-${pad1(printedSchema.replaceAll('`', "'"))}
+${scheme}
 \`;
-`
+`}
