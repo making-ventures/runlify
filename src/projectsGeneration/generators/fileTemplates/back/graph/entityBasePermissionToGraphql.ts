c3a2541966450d9a58e54b63d11b237d379c762c
Tebuchet <tebuchet@gmail.com>
2023-07-12 08:48:47 +0300

    fix padding

--- src/projectsGeneration/generators/fileTemplates/back/graph/entityBasePermissionToGraphql.ts
+++ src/projectsGeneration/generators/fileTemplates/back/graph/entityBasePermissionToGraphql.ts
@@ -22,16 +22,17 @@ ${
 }
 const ${
     entity.name
-  }BasePermissionToGraphql: Partial<PermissionToGraphql<${pascalPlural(
+  }BasePermissionToGraphql:
+  Partial<PermissionToGraphql<${pascalPlural(
     entity.name
   )}Service>> = {
-  meta: '_all${pascalPlural(entity.name)}Meta',
-  get: '${pascalSingular(entity.name)}',
-  all: 'all${pascalPlural(entity.name)}',
-  create: 'create${pascalSingular(entity.name)}',
-  update: 'update${pascalSingular(entity.name)}',
-  delete: 'remove${pascalSingular(entity.name)}',
-};
+    meta: '_all${pascalPlural(entity.name)}Meta',
+    get: '${pascalSingular(entity.name)}',
+    all: 'all${pascalPlural(entity.name)}',
+    create: 'create${pascalSingular(entity.name)}',
+    update: 'update${pascalSingular(entity.name)}',
+    delete: 'remove${pascalSingular(entity.name)}',
+  };
 
 export default ${entity.name}BasePermissionToGraphql;
 `
--- src/projectsGeneration/generators/fileTemplates/back/graph/resolvers.ts
+++ src/projectsGeneration/generators/fileTemplates/back/graph/resolvers.ts
@@ -35,42 +35,48 @@ const queryResolvers: Resolvers = {
   Query: {
     ${pascalSingular(entity.name)}: (_, {id}, {context}: {context: Context}) =>
       context.service('${camelPlural(entity.name)}').get(id),
-    all${pascalPlural(entity.name)}: (_, params: QueryAll${pascalPlural(
-  entity.name
-)}Args, {context}: {context: Context}) =>
-      context.service('${camelPlural(entity.name)}').all(params),
-    _all${pascalPlural(entity.name)}Meta: (_, params: Query_All${pascalPlural(
-  entity.name
-)}MetaArgs, {context}: {context: Context}) =>
-      context.service('${camelPlural(entity.name)}').meta(params),
+    all${pascalPlural(entity.name)}:
+      (_, params: QueryAll${pascalPlural(
+    entity.name
+  )}Args, {context}: {context: Context}) =>
+        context.service('${camelPlural(entity.name)}').all(params),
+    _all${pascalPlural(entity.name)}Meta:
+      (_, params: Query_All${pascalPlural(
+    entity.name
+  )}MetaArgs, {context}: {context: Context}) =>
+        context.service('${camelPlural(entity.name)}').meta(params),
   },
   Mutation: {
     create${pascalSingular(
       entity.name
-    )}: (_, params: MutationCreate${pascalSingular(
-  entity.name
-)}Args, {context}: {context: Context}) =>
-      context.service('${camelPlural(entity.name)}').create(params, true),
+    )}:
+      (_, params: MutationCreate${pascalSingular(
+    entity.name
+  )}Args, {context}: {context: Context}) =>
+        context.service('${camelPlural(entity.name)}').create(params, true),
     update${pascalSingular(
       entity.name
-    )}: (_, params: MutationUpdate${pascalSingular(
-  entity.name
-)}Args, {context}: {context: Context}) =>
-      context.service('${camelPlural(entity.name)}').update(params, true),
+    )}:
+      (_, params: MutationUpdate${pascalSingular(
+    entity.name
+  )}Args, {context}: {context: Context}) =>
+        context.service('${camelPlural(entity.name)}').update(params, true),
     remove${pascalSingular(
       entity.name
-    )}: (_, params: MutationRemove${pascalSingular(
-  entity.name
-)}Args, {context}: {context: Context}) =>
-      context.service('${camelPlural(entity.name)}').delete(params),${
+    )}:
+      (_, params: MutationRemove${pascalSingular(
+    entity.name
+  )}Args, {context}: {context: Context}) =>
+        context.service('${camelPlural(entity.name)}').delete(params),${
   entity.type === 'document'
     ? `
     rePost${pascalSingular(
       entity.name
-    )}: (_, params: MutationRePost${pascalSingular(
-        entity.name
-      )}Args, {context}: {context: Context}) =>
-      context.service('${camelPlural(entity.name)}').rePost(params.id),`
+    )}:
+      (_, params: MutationRePost${pascalSingular(
+          entity.name
+        )}Args, {context}: {context: Context}) =>
+        context.service('${camelPlural(entity.name)}').rePost(params.id),`
     : ''
 }
   },
