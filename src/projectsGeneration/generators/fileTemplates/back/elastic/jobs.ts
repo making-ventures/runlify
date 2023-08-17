fe63e255a82ae341f2bf6a6d7581e2ee56bfe23c
yurii_papka <papka.yurii@gmail.com>
2023-08-18 00:41:13 +0300

    schema for bootstrapping elastic indexes

---
+++ src/projectsGeneration/generators/fileTemplates/back/elastic/jobs.ts
@@ -0,0 +1,61 @@
+import {Entity, Field} from '../../../../builders'
+import * as R from 'ramda'
+import {pascalSingular} from '../../../../../utils/cases';
+import {defaultBootstrapEntityOptions} from '../../../../types';
+import {generatedWarning} from '../../../../utils';
+
+type GroupedByType = Partial<Record<Field['type'] | 'keyword', string[]>>
+
+const getGroupedByType = (e: Entity) => e.fields.reduce<GroupedByType>((acc, field) => {
+  const isIdField = ['id', 'link'].includes(field.category)
+  let type = isIdField ? 'keyword' : field.type
+
+  if (type === 'datetime') {
+    type = 'date'
+  }
+
+  if(!R.is(Array, acc[type])) {
+    acc[type] = []
+  }
+
+  (acc[type] as string[]).push(field.name)
+
+  return acc
+}, {})
+
+const getConstructor = (g: string) => {
+  switch (g) {
+    case 'keyword':
+      return 'keywordFields'
+    case 'datetime':
+    case 'date':
+      return 'dateFields'
+    case 'bool':
+      return 'booleanFields'
+    case 'int':
+      return 'integerFields'
+    case 'string':
+      return 'textFields'
+    default:
+      throw new Error(`Unknown group identifier ${g}`)
+  }
+}
+
+export const genJobsTmpl = (entities: Entity[], options = defaultBootstrapEntityOptions) => {
+  return `import Entity from '../../types/Entity';
+import {textFields, keywordFields, integerFields, dateFields, booleanFields} from './utils';
+import {ElasticJobs} from './type';
+${options.skipWarningThisIsGenerated
+      ? ''
+      : `
+// ${generatedWarning}
+`}
+export const genJobs: ElasticJobs = {
+  ${entities.map((e) => `[Entity.${pascalSingular(e.name)}]: {
+    ${Object.entries(getGroupedByType(e)).map(([type, fields]) => `...${getConstructor(type)}([
+      ${fields.map(f => `'${f}',`).join('\n      ')}
+    ]),`).join('\n    ')}
+  },`).join('\n  ')}
+};
+`;
+}
