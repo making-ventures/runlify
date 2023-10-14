9251c611816415d3f76729dbb01ddd158be26a99
Damir <tebuchet@gmail.com>
2023-10-14 23:57:11 +0300

    auth

--- src/projectsGeneration/builders/SystemMetaBuilder.ts
+++ src/projectsGeneration/builders/SystemMetaBuilder.ts
@@ -327,12 +327,14 @@ class SystemMetaBuilder {
     // oidc.app.url: 'https://kk.stage01.making.ventures',
     // oidc.app.realm: 'cmng-dev',
     // oidc.app.clientId: 'cmng-admin',
-    this.addConfigVar('oidc.adm.url', 'string', false, undefined, 'Хост oidc для админки');
-    this.addConfigVar('oidc.adm.realm', 'string', false, undefined, 'Реалм oidc для админки');
-    this.addConfigVar('oidc.adm.clientId', 'string', false, undefined, 'Идентификатор клиента oidc для админки');
-    this.addConfigVar('oidc.app.url', 'string', false, undefined, 'Хост oidc для приложения пользователя');
-    this.addConfigVar('oidc.app.realm', 'string', false, undefined, 'Реалм oidc для приложения пользователя');
-    this.addConfigVar('oidc.app.clientId', 'string', false, undefined, 'Идентификатор клиента oidc для приложения пользователя');
+    this.addConfigVar('oidc.adm.url', 'string', false, undefined, 'Хост oidc для админки', ['back', 'admin-app']);
+    this.addConfigVar('oidc.adm.realm', 'string', false, undefined, 'Реалм oidc для админки', ['back', 'admin-app']);
+    this.addConfigVar('oidc.adm.clientId', 'string', false, undefined, 'Идентификатор клиента oidc для админки', ['back', 'admin-app']);
+    this.addConfigVar('oidc.app.url', 'string', false, undefined, 'Хост oidc для приложения пользователя', ['back', 'admin-app']);
+    this.addConfigVar('oidc.app.realm', 'string', false, undefined, 'Реалм oidc для приложения пользователя', ['back', 'admin-app']);
+    this.addConfigVar('oidc.app.clientId', 'string', false, undefined, 'Идентификатор клиента oidc для приложения пользователя', ['back', 'admin-app']);
+
+    // 'admin-app'
 
     this.addDeployEnvironment({
       name: 'dev',
