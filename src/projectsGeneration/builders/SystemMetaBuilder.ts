7951892be58c48cfa114df41239c2c899c881351
Damir <tebuchet@gmail.com>
2023-10-16 17:18:06 +0300

    keycloak.app.cli

--- src/projectsGeneration/builders/SystemMetaBuilder.ts
+++ src/projectsGeneration/builders/SystemMetaBuilder.ts
@@ -328,9 +328,9 @@ class SystemMetaBuilder {
     this.addConfigVar('oidc.app.realm', 'string', false, undefined, 'Реалм oidc для приложения пользователя', ['back', 'cutomer-app']);
     this.addConfigVar('oidc.app.clientId', 'string', false, undefined, 'Идентификатор клиента oidc для приложения пользователя', ['cutomer-app']);
 
-    this.addConfigVar('keycloak.adm.cli.username', 'string', false, undefined, 'Пользователь для доступа в api keycloak админки');
+    this.addConfigVar('keycloak.adm.cli.username', 'string', false, 'admin-cli', 'Пользователь для доступа в api keycloak админки');
     this.addConfigVar('keycloak.adm.cli.password', 'string', false, undefined, 'Пароль для доступа в api keycloak админки');
-    this.addConfigVar('keycloak.app.cli.username', 'string', false, undefined, 'Пользователь для доступа в api keycloak приложения пользователя');
+    this.addConfigVar('keycloak.app.cli.username', 'string', false, 'admin-cli', 'Пользователь для доступа в api keycloak приложения пользователя');
     this.addConfigVar('keycloak.app.cli.password', 'string', false, undefined, 'Пароль для доступа в api keycloak приложения пользователя');
 
     // 'admin-app'
