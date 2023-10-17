f31f038a4d395b9a799df77867ee09d9e92d2859
Tebuchet <tebuchet@gmail.com>
2023-10-17 19:39:03 +0300

    backend-api

--- src/projectsGeneration/builders/SystemMetaBuilder.ts
+++ src/projectsGeneration/builders/SystemMetaBuilder.ts
@@ -328,9 +328,9 @@ class SystemMetaBuilder {
     this.addConfigVar('oidc.app.realm', 'string', false, undefined, 'Реалм oidc для приложения пользователя', ['back', 'cutomer-app']);
     this.addConfigVar('oidc.app.clientId', 'string', false, undefined, 'Идентификатор клиента oidc для приложения пользователя', ['cutomer-app']);
 
-    this.addConfigVar('keycloak.adm.cli.client', 'string', false, 'admin-cli', 'Клиент для доступа в api keycloak админки');
+    this.addConfigVar('keycloak.adm.cli.client', 'string', false, 'backend-api', 'Клиент для доступа в api keycloak админки');
     this.addConfigVar('keycloak.adm.cli.secret', 'string', false, undefined, 'Секрет для доступа в api keycloak админки');
-    this.addConfigVar('keycloak.app.cli.client', 'string', false, 'admin-cli', 'Клиент для доступа в api keycloak приложения пользователя');
+    this.addConfigVar('keycloak.app.cli.client', 'string', false, 'backend-api', 'Клиент для доступа в api keycloak приложения пользователя');
     this.addConfigVar('keycloak.app.cli.secret', 'string', false, undefined, 'Секрет для доступа в api keycloak приложения пользователя');
 
     // 'admin-app'
