20be80c85bab52de6ce63b515e39520331ead907
Dmitry Yarotski <d.yarotski@outlook.com>
2024-01-02 15:29:14 +0300

    ci-notify

--- src/projectsGeneration/generators/fileTemplates/ui/environment/ciNotify.ts
+++ src/projectsGeneration/generators/fileTemplates/ui/environment/ciNotify.ts
@@ -18,8 +18,8 @@ DISCORD_URL="$NOTIFY_DISCORD_WEBHOOK_URL"
 TG_TEXT="Status: $1%0A%0AProject: $CI_PROJECT_NAME%0AURL: $CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch: $CI_COMMIT_REF_SLUG"
 DISCORD_TEXT="Status: **$1**\\nProject: **$CI_PROJECT_NAME**\\nURL: $CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/\\nBranch: $CI_COMMIT_REF_SLUG\\n\\n"
 
-# Отправка уведомления в Telegram
+# Telegram notification
 curl -s --max-time $TIME -d "chat_id=$NOTIFY_TELEGRAM_CHAT_ID&disable_web_page_preview=1&text=$TG_TEXT" $TELEGRAM_URL > /dev/null
 
-# Отправка уведомления в Discord
+# Discord notification
 curl -s --max-time $TIME -H "Content-Type: application/json" -d "{\\"content\\":\\"$DISCORD_TEXT\\",\\"allowed_mentions\\":{\\"parse\\":[]}}" $DISCORD_URL > /dev/null`
