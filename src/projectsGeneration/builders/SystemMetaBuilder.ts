84f705d1b243fb444287acc82dc0f35568899ba9
Tebuchet <tebuchet@gmail.com>
2024-03-11 14:25:59 +0300

    jwksUri

--- src/projectsGeneration/builders/SystemMetaBuilder.ts
+++ src/projectsGeneration/builders/SystemMetaBuilder.ts
@@ -340,6 +340,7 @@ class SystemMetaBuilder implements MethodsModelsHolder {
     // oidc
     this.addConfigVar('oidc.adm.url', 'string', false, undefined, 'Хост oidc для админки', ['back', 'admin-app']);
     this.addConfigVar('oidc.adm.realm', 'string', false, undefined, 'Реалм oidc для админки', ['back', 'admin-app']);
+    this.addConfigVar('oidc.adm.jwksUri', 'string', false, undefined, 'Uri jwks oidc для проверки авторизации админки', ['back']);
     this.addConfigVar('oidc.adm.issuer', 'string', false, undefined, 'Issuer oidc для админки', ['admin-app']);
     this.addConfigVar('oidc.adm.logoutType', 'string', false, undefined, 'Тип выхода админки', ['admin-app']);
     this.addConfigVar('oidc.adm.clientId', 'string', false, undefined, 'Идентификатор клиента oidc для админки', ['admin-app']);
