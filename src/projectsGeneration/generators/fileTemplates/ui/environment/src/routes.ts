9951c56865f8c3f8c945289150cfb32af24507d0
Dmitry Yarotski <d.yarotski@outlook.com>
2024-04-15 11:38:13 +0300

    comment route

--- src/projectsGeneration/generators/fileTemplates/ui/environment/src/routes.ts
+++ src/projectsGeneration/generators/fileTemplates/ui/environment/src/routes.ts
@@ -21,7 +21,7 @@ ${
 }
 const LoadableDashboard = Loadable(() => import('./Dashboard'));
 const LoadableFunctions = Loadable(() => import('./functions/Functions'));
-const LoadableResourcesPage = Loadable(() => import('./ResourcesPage'));
+// const LoadableResourcesPage = Loadable(() => import('./ResourcesPage'));
 const LoadableMetaPage = Loadable(() => import('./MetaPage'));
 const LoadableDebugPage = Loadable(() => import('./utility/DebugPage'));${system.pages.length ? `
 ${system.pages.map(p => `const Loadable${pascalCase(p.name)} = Loadable(() => import('./standalonePages/${pascalCase(p.name)}/${pascalCase(p.name)}'));`).join('\n')}` : ''}
@@ -29,7 +29,7 @@ ${system.pages.map(p => `const Loadable${pascalCase(p.name)} = Loadable(() => im
 export const routes = [
   <Route element={<Guard shouldHave='dashboard'><LoadableDashboard /></Guard>} key='dashboard' path='/dashboard' />,
   <Route element={<Guard shouldHave='functions'><LoadableFunctions /></Guard>} key='functions' path='/functions' />,
-  <Route element={<Guard shouldHave='resources'><LoadableResourcesPage /></Guard>} key='resources' path='/resources' />,
+  // <Route element={<Guard shouldHave='resources'><LoadableResourcesPage /></Guard>} key='resources' path='/resources' />,
   <Route element={<Guard shouldHave='meta'><LoadableMetaPage /></Guard>} key='meta' path='/meta' />,
   <Route element={<Guard shouldHave='debug'><LoadableDebugPage /></Guard>} key='debug' path='/debug' />,
 ${system.pages.map(p => `  <Route element={<Loadable${pascalCase(p.name)} />} key='${p.name}' path='${p.link}' />,`).join('\n')}
