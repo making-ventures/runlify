8c47696360d484051cfe74bb2d3d95c5fe807089
leshenkov <lvn.new@gmail.com>
2025-05-31 15:00:26 +0300

    notify

---
+++ ci-notify.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TIME="10"
+TELEGRAM_URL="https://api.telegram.org/bot$NOTIFY_TELEGRAM_BOT_TOKEN/sendMessage"
+DISCORD_URL="$NOTIFY_DISCORD_WEBHOOK_URL"
+
+# Get Runlify version
+RUNLIFY_VERSION=$(runlify --version 2>/dev/null || echo "Unknown")
+
+TG_TEXT="Status: $1%0A%0AProject: $CI_PROJECT_NAME%0AURL: $CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch: $CI_COMMIT_REF_SLUG%0ARunlify Version: $RUNLIFY_VERSION"
+DISCORD_TEXT="Status: **$1**\nProject: **$CI_PROJECT_NAME**\nURL: $CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/\nBranch: $CI_COMMIT_REF_SLUG\nRunlify Version: $RUNLIFY_VERSION\n\n"
+
+# Telegram notification
+curl -s --max-time $TIME -d "chat_id=$NOTIFY_TELEGRAM_CHAT_ID&disable_web_page_preview=1&text=$TG_TEXT" $TELEGRAM_URL > /dev/null
+
+# Discord notification
+curl -s --max-time $TIME -H "Content-Type: application/json" -d "{\"content\":\"$DISCORD_TEXT\",\"allowed_mentions\":{\"parse\":[]}}" $DISCORD_URL > /dev/null
