c29ff6f6963303f49ad61650135a5274638e3aa4
tebuchet <tebuchet@gmail.com>
2025-06-22 17:46:06 +0300

    breadcumbs in list

---
+++ src/projectsGeneration/generators/fileTemplates/ui/pages/EntityList/EntityBreadcrumbs.ts
@@ -0,0 +1,16 @@
+import { pascalSingular } from '../../../../../../utils/cases'
+import { EntityWideGenerationArgs } from '../../../../../args'
+
+export const uiListBreadcrumbsTmpl = ({ entity }: EntityWideGenerationArgs) => {
+  return `import {FC} from 'react';
+// import {Breadcrumbs} from '../../../../raUiLib/Breadcrumbs';
+
+const ${pascalSingular(entity.name)}ListBreadcrumbs: FC = () => null;
+
+// const ${pascalSingular(entity.name)}ListBreadcrumbs: FC = () => (
+//   <Breadcrumbs />
+// );
+
+export default ${pascalSingular(entity.name)}ListBreadcrumbs;
+`
+}
